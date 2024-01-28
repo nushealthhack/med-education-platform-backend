06102fd47617eddf5258f3e4a702c5e0effba601
Randyyeo <randallysk@gmail.com>
2024-01-28 15:56:45 +0800

    nit removal

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import math
 import sys
+import os
 sys.prefix = 'C:/Users/randa/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0/LocalCache/local-packages'
 from ironpdf import *
 
@@ -17,7 +18,6 @@ def home():
 
 @app.route('/upload-pdf', methods=["POST"])
 def generate_document():
-  print(request.files)
   if 'file' not in request.files:
     return 'No file part'
 
@@ -30,9 +30,16 @@ def generate_document():
   file.save('uploads/' + file.filename)
   pdf = PdfDocument.FromFile("./uploads/" + file.filename)
   # Extract all pages to a folder as image files
-  pdf.RasterizeToImageFiles(f"images/{file.filename}_*.png",DPI=96)
+  pdf.RasterizeToImageFiles(f"images/{file.filename}/{file.filename}*.png",DPI=96)
 
   return 'File uploaded successfully!'
+
+@app.route('./get-folders')
+def get_all_folders():
+  items = os.listdir('./images')
+  folders = [item for item in items if os.path.isdir(os.path.join(path, item))]
+  return folders
+
     
 if __name__ == '__main__':
     app.run(debug=True)
